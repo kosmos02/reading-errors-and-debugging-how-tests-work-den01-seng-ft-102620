e64f020b897d486319715f34bede84e1ddf88391
Alexander Gabriel <alexandrgabe@gmail.com>
2020-09-21 22:34:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@
 first_number = 2 
 second_number = 1 
 sum = first_number + second_number
-difference = first_number + second_number
+difference = first_number - second_number
 
 #product = 
 #quotient =
